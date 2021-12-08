2660f9892873feec5c79d0e193aeeb6e3593075a
Radminas Usackas <usackasr@gmail.com>
2021-12-08 12:41:34 +0000

    Added article module

---
+++ models/article.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    author: { type: String, required: true, minlength: 1, maxlength: 100 },
+    article: { type: String, required: true },
+    date: { type: Date, required: true }
+});
+
+ArticleSchema.virtual('delete_url')
+    .get(function () {
+        return 'article/' + this._id;
+    });
+
+module.exports = mongoose.model('Article', ArticleSchema, 'articles');

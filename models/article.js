ca3d514863e9dae963cdb4a1aad712ea098d2c53
Radminas Usackas <usackasr@gmail.com>
2021-12-09 16:37:18 +0000

    Added title to article model

--- models/article.js
+++ models/article.js
@@ -3,13 +3,14 @@ var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
     author: { type: String, required: true, minlength: 1, maxlength: 100 },
+    title: { type: String, required: true, minlength: 1, maxlength: 100 },
     article: { type: String, required: true },
     date: { type: Date, required: true }
 });
 
 ArticleSchema.virtual('url')
     .get(function () {
-        return 'article/' + this._id;
+        return '/articles/' + this._id;
     });
 
 module.exports = mongoose.model('Article', ArticleSchema, 'articles');

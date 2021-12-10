e5cff0f11060626806cf1d0d65ca7814a05a3b4f
Radminas Usackas <usackasr@gmail.com>
2021-12-10 20:07:35 +0000

    Removed cache

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var cacheService = require("express-api-cache");
-var cache = cacheService.cache;
 var articleController = require('../controllers/articleController');
 var commentController = require('../controllers/commentController');
 
 // Article routes
-router.get('/articles', cache("10 seconds"), articleController.all_articles_get);
+router.get('/articles', articleController.all_articles_get);
 router.post('/post_article', articleController.article_post);
 router.get('/articles/:id', articleController.article_get);
 router.put('/articles/:id', articleController.article_put);
@@ -14,7 +12,7 @@ router.delete('/articles/:id', articleController.article_delete);
 
 
 // Comment routes
-router.get('/comments/:id', cache("10 seconds"), commentController.all_comments_get);
+router.get('/comments/:id', commentController.all_comments_get);
 router.post('/post_comment/:id', commentController.comment_post);
 
 module.exports = router;

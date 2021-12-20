39ee6a6f218d52ec774d7672f82cbb50b1d0cd12
ne0rad <usackasr@gmail.com>
2021-12-20 16:29:34 +0000

    Updated user model

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
     username: { type: String, required: true, minlength: 1, maxlength: 100 },
     password: { type: String, required: true, minlength: 1, maxlength: 100 },
+    role: { type: Number, required: true },
     token: { type: String, required: true }
 });
 
